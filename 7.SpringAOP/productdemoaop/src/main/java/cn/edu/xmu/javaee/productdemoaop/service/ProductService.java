328adfca9880f079e16b572527a122620f5f05a1
rickey-c <1604120019@qq.com>
2024-11-29 15:13:44 +0800

    refactor(ProductService、ProductDao): 转移全部缓存查询逻辑到Dao层

--- 7.SpringAOP/productdemoaop/src/main/java/cn/edu/xmu/javaee/productdemoaop/service/ProductService.java
+++ 7.SpringAOP/productdemoaop/src/main/java/cn/edu/xmu/javaee/productdemoaop/service/ProductService.java
@@ -25,11 +25,6 @@ public class ProductService {
 
     private ProductDao productDao;
 
-    private final String PRODUCT_CACHE_KEY = "product:";
-
-    private final Long CACHE_TIME_OUT = 100L;
-    
-
     @Autowired
     public ProductService(ProductDao productDao) {
         this.productDao = productDao;
@@ -42,31 +37,8 @@ public class ProductService {
      * @return 商品对象
      */
     @Transactional(rollbackFor = {BusinessException.class})
-    public Product retrieveProductByID(Long id, boolean all,boolean useRedis) throws BusinessException {
-        logger.debug("findProductById: id = {}, all = {}", id, all);
-        if (useRedis){
-            String cacheKey = PRODUCT_CACHE_KEY + id;
-            // 查缓存
-            Product product = (Product)redisUtil.get(cacheKey);
-            if (product!=null){
-                logger.info("Hit product: key = {}",cacheKey);
-                return product;
-            }
-            // 查数据库
-            product = productDao.retrieveProductByID(id, all, true);
-            if (product!=null){
-                // 存入缓存
-                boolean setResult = redisUtil.set(cacheKey, product, CACHE_TIME_OUT);
-                logger.info("Store product: key = {},result = {}",cacheKey,setResult);
-            }
-            return product;
-        }
-        else {
-            // 查数据库
-            Product product = productDao.retrieveProductByID(id, all, false);
-            return product;
-        }
-        // return productDao.retrieveProductByID(id, all);
+    public Product retrieveProductByID(Long id, boolean all, boolean useRedis) throws BusinessException {
+        return productDao.retrieveProductByID(id, all, useRedis);
     }
 
     /**
@@ -76,31 +48,35 @@ public class ProductService {
      * @return 商品对象
      */
     @Transactional
-    public List<Product> retrieveProductByName(String name, boolean all) throws BusinessException{
+    public List<Product> retrieveProductByName(String name, boolean all) throws BusinessException {
         return productDao.retrieveProductByName(name, all);
     }
 
     /**
      * 新增商品
+     *
      * @param product 新商品信息
      * @return 新商品
      */
     @Transactional
-    public Product createProduct(Product product, User user) throws BusinessException{
+    public Product createProduct(Product product, User user) throws BusinessException {
         return productDao.createProduct(product, user);
     }
 
 
     /**
      * 修改商品
+     *
      * @param product 修改商品信息
      */
     @Transactional
-    public void modifyProduct(Product product, User user) throws BusinessException{
+    public void modifyProduct(Product product, User user) throws BusinessException {
         productDao.modiProduct(product, user);
     }
 
-    /** 删除商品
+    /**
+     * 删除商品
+     *
      * @param id 商品id
      * @return 删除是否成功
      */
@@ -122,7 +98,7 @@ public class ProductService {
      * @return 商品对象
      */
     @Transactional
-    public List<Product> findProductByName_manual(String name) throws BusinessException{
+    public List<Product> findProductByName_manual(String name) throws BusinessException {
         return productDao.findProductByName_manual(name);
     }
 
